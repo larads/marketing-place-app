a7480f213c2013a60e04fbe95bdc1ec7b0ec2a8b
larads <dasilval.mariana@gmail.com>
2025-10-18 11:09:23 -0400

    feat: implement login and registration views with validation schemas

---
+++ src/view-models/register/register.view.tsx
@@ -0,0 +1,81 @@
+
+import { FC } from 'react'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { AppButton } from '../../shared/components/AppButton'
+import { AppInputController } from '../../shared/components/AppInputController'
+import { AuthFormHeader } from '../../shared/components/AuthFormHeader'
+import { KeyboardContainer } from '../../shared/components/KeyboardContainer'
+import { useRegisterViewModel } from './useRegister.view'
+import { router } from 'expo-router'
+
+export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
+  onSubmit,
+  control,
+}) => {
+  return (
+    <KeyboardContainer>
+      <ScrollView className="flex-1 px-[40px]">
+        <AuthFormHeader
+          title="Crie sua conta"
+          subTitle="Informe seus dados pessoais e de acesso"
+        />
+        <AppInputController
+          leftIcon="person-outline"
+          label="NOME"
+          control={control}
+          name="name"
+          placeholder="Seu nome completo"
+        />
+
+        <AppInputController
+          leftIcon="call-outline"
+          label="TELEFONE"
+          control={control}
+          name="phone"
+          placeholder="(00) 00000-0000"
+        />
+
+        <Text className="text-base mt-6 font-bold text-gray-500">Acesso</Text>
+
+        <AppInputController
+          leftIcon="mail-outline"
+          label="E-MAIL"
+          control={control}
+          name="email"
+          placeholder="mail@example.com.br"
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="SENHA"
+          control={control}
+          name="password"
+          placeholder="Sua senha"
+          secureTextEntry
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="CONFIRMAR SENHA"
+          control={control}
+          name="confirmPassword"
+          placeholder="Confirme sua senha"
+          secureTextEntry
+        />
+
+        <AppButton className="mt-6" onPress={onSubmit}>
+          Registrar
+        </AppButton>
+
+        <View className="mt-16">
+          <Text className="text-base mb-6 text-gray-300">
+            Já tem uma conta?
+          </Text>
+          <AppButton variant="outlined" onPress={() => router.push('/login')}>
+            Login
+          </AppButton>
+        </View>
+      </ScrollView>
+    </KeyboardContainer>
+  )
+}
---
+++ src/view-models/register/useRegister.view.ts
@@ -0,0 +1,47 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { RegisterFormData, registerScheme } from './register.schema'
+import { useRegisterMutation } from '../../shared/queries/auth/use-register.mutation'
+import { useUserStore } from '../../shared/store/user-store'
+
+export const useRegisterViewModel = () => {
+    const userRegisterMutation = useRegisterMutation()
+    const { setSession, user } = useUserStore()
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<RegisterFormData>({
+        resolver: yupResolver(registerScheme),
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            phone: '',
+        },
+    })
+
+
+    const onSubmit = handleSubmit(async (userData) => {
+        console.log(userData)
+        const { confirmPassword, ...registerData } = userData
+        const mutationResponse =
+            await userRegisterMutation.mutateAsync(registerData)
+
+        setSession({
+            refreshToken: mutationResponse.refreshToken,
+            token: mutationResponse.token,
+            user: mutationResponse.user,
+        })
+    })
+
+    console.log(user)
+
+    return {
+        control,
+        errors,
+        onSubmit,
+    }
+}
