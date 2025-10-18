7d971adbadb2164cc3ed6c7de3ecebc6ef117f97
larads <dasilval.mariana@gmail.com>
2025-10-18 11:09:09 -0400

    feat: add reusable components for authentication forms and input handling

---
+++ src/shared/components/AppInput/index.tsx
@@ -0,0 +1,99 @@
+import { Ionicons } from '@expo/vector-icons'
+import { FC } from 'react'
+import {
+    Pressable,
+    Text,
+    TextInput,
+    TextInputProps,
+    TouchableOpacity,
+    View,
+} from 'react-native'
+import { AppInputVariantsProps, appInputVariants } from './input.variants'
+import { useAppInputViewModel } from './useAppInputViewModel'
+
+export interface AppInputProps extends TextInputProps, AppInputVariantsProps {
+    label?: string
+    leftIcon?: keyof typeof Ionicons.glyphMap
+    rightIcon?: keyof typeof Ionicons.glyphMap
+    containerClassName?: string
+    mask?: (value: string) => void | string
+    error?: string
+}
+
+export const AppInput: FC<AppInputProps> = ({
+    label,
+    leftIcon,
+    rightIcon,
+    containerClassName,
+    mask,
+    value,
+    isError,
+    secureTextEntry,
+    onBlur,
+    onFocus,
+    onChangeText,
+    error,
+    isDisabled,
+    ...textInputProps
+}) => {
+    const {
+        getIconColor,
+        showPassword,
+        handlePasswordToggle,
+        handleFocus,
+        handleBlur,
+        handleTextChange,
+        isFocused,
+    } = useAppInputViewModel({
+        error,
+        onBlur,
+        onFocus,
+        isError: !!error,
+        mask,
+        onChangeText,
+        isDisabled,
+        secureTextEntry,
+        value,
+    })
+    const styles = appInputVariants({ isFocused })
+
+    return (
+        <View className={styles.container({ className: containerClassName })}>
+            <Text className={styles.label()}>{label}</Text>
+            <Pressable className={styles.wrapper()}>
+                {leftIcon && (
+                    <Ionicons
+                        color={getIconColor()}
+                        className="mr-3"
+                        size={22}
+                        name={leftIcon}
+                    />
+                )}
+
+                <TextInput
+                    onBlur={handleBlur}
+                    onFocus={handleFocus}
+                    value={value}
+                    onChangeText={handleTextChange}
+                    secureTextEntry={showPassword}
+                    className={styles.input()}
+                    {...textInputProps}
+                />
+                {secureTextEntry && (
+                    <TouchableOpacity activeOpacity={0.7} onPress={handlePasswordToggle}>
+                        <Ionicons
+                            size={22}
+                            name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+                        />
+                    </TouchableOpacity>
+                )}
+            </Pressable>
+
+            {error && (
+                <Text className={styles.error()}>
+                    <Ionicons className="ml-2" name="alert-circle-outline" /> {error}
+                </Text>
+            )}
+        </View>
+    )
+}
